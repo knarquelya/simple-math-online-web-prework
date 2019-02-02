51257f14282934f09123fcd4382d55be8bbb1b82
Knarquelya O'Neal <knarquelya.oneal@gmail.com>
2019-02-02 21:17:06 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,27 @@
 def addition(num1, num2)
+  puts 5 + 4 = 9
 end
 
 def subtraction(num1, num2)
+  puts 10 - 5 = 5
 end
 
 def division(num1, num2)
+  puts 50 / 2 = 25
 end
 
 def multiplication(num1, num2)
+  puts 4 * 30 = 120
 end
 
 def modulo(num1, num2)
+  puts 34 % 5 = 4
 end
 
 def square_root(num)
+  puts Math.sqrt(81) = 9
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  puts 7 + ((43 * 23) / 8) = 18
 end
